09012be11089fd18bdae13902338f99fe81144b2
FacuBarrios01 <facubarrios@gmail.com>
2022-12-26 00:20:53 +0100

    Solution part 1 day 3

--- day3/sol1.py
+++ day3/sol1.py
@@ -1,10 +1,21 @@
+import string
+
 with open("./input.in") as fin:
     data = fin.read().strip().split()
 
 data_leng = len(data)
+sum = 0
 
-for i in range(0, 3):
+def calculate_points(letter):
+    letters = list(string.ascii_lowercase + string.ascii_uppercase)
+    points = dict(zip(letters, range(1, 53)))
+    return points.get(letter)
+
+
+for i in range(0, data_leng):
     line = data[i]
     line_length = len(line)//2
     first_half = set(line[:line_length])
-    print(first_half)
+    match = first_half.intersection(line[line_length:]).pop()
+    sum += calculate_points(match)
+print(sum)
